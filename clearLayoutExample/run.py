1589089a3162496b37171cb78a48a36b0fae18e5
thinuser <thinuser@LWT509a4c05d009.XPESQA.COM>
2018-08-06 09:28:01 -0700

    Example of clearing Layout

---
+++ clearLayoutExample/run.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'demo.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(250, 140, 361, 341))
+        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        
+
+	"""self.formLayout = QtGui.QFormLayout()
+        self.formLayout.setObjectName(_fromUtf8("formLayout"))
+	self.label = QtGui.QLabel(self.verticalLayoutWidget)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.formLayout.setWidget(0, QtGui.QFormLayout.LabelRole, self.label)
+        self.lineEdit = QtGui.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.formLayout.setWidget(0, QtGui.QFormLayout.FieldRole, self.lineEdit)
+      """  
+
+
+	#------------
+	self.formLayout_1 = QtGui.QFormLayout()
+        self.formLayout_1.setObjectName(_fromUtf8("formLayout"))
+	self.label_1 = QtGui.QLabel(self.verticalLayoutWidget)
+        self.label_1.setObjectName(_fromUtf8("label"))
+        self.formLayout_1.setWidget(0, QtGui.QFormLayout.LabelRole, self.label_1)
+        self.lineEdit = QtGui.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.formLayout_1.setWidget(0, QtGui.QFormLayout.FieldRole, self.lineEdit)
+	#--------------
+
+
+
+	self.verticalLayout.addLayout(self.formLayout_1)
+
+
+
+
+        self.pushButton = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.verticalLayout.addWidget(self.pushButton)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+
+	#calling clear layout function
+	self.pushButton.clicked.connect(self.clearLayout)
+
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        #self.label.setText(_translate("MainWindow", "Username", None))
+	self.label_1.setText(_translate("MainWindow", "Password", None))
+        self.pushButton.setText(_translate("MainWindow", "OK", None))
+
+
+    #function for clearing layout
+    def clearLayout(self):
+	layout = self.formLayout_1
+	while layout.count()>0:
+		item = layout.takeAt(0)
+		if not item:
+			continue
+
+		w = item.widget()
+		if w:
+			w.deleteLater()
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
