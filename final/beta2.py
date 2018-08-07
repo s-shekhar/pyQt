50c4b2c8608089ec8b48775ce80b265a752c410f
thinuser <thinuser@LWT509a4c05d009.XPESQA.COM>
2018-08-07 08:56:24 -0700

    Final UI build with comboBox

---
+++ final/beta2.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(743, 522)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Ubuntu"))
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.tabWidget = QtGui.QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
+        self.tab = QtGui.QWidget()
+        self.tab.setObjectName(_fromUtf8("tab"))
+        self.label_2 = QtGui.QLabel(self.tab)
+        self.label_2.setGeometry(QtCore.QRect(10, 10, 211, 20))
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Ubuntu"))
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.line = QtGui.QFrame(self.tab)
+        self.line.setGeometry(QtCore.QRect(0, 40, 721, 16))
+        self.line.setFrameShape(QtGui.QFrame.HLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName(_fromUtf8("line"))
+        self.tabWidget.addTab(self.tab, _fromUtf8(""))
+        self.tab_2 = QtGui.QWidget()
+        self.tab_2.setObjectName(_fromUtf8("tab_2"))
+        self.label_3 = QtGui.QLabel(self.tab_2)
+        self.label_3.setGeometry(QtCore.QRect(10, 10, 221, 20))
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Ubuntu"))
+        font.setPointSize(12)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.line_2 = QtGui.QFrame(self.tab_2)
+        self.line_2.setGeometry(QtCore.QRect(0, 40, 721, 16))
+        self.line_2.setFrameShape(QtGui.QFrame.HLine)
+        self.line_2.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line_2.setObjectName(_fromUtf8("line_2"))
+        self.tabWidget.addTab(self.tab_2, _fromUtf8(""))
+        self.tab_3 = QtGui.QWidget()
+        self.tab_3.setObjectName(_fromUtf8("tab_3"))
+        self.label_4 = QtGui.QLabel(self.tab_3)
+        self.label_4.setGeometry(QtCore.QRect(10, 10, 201, 20))
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Ubuntu"))
+        font.setPointSize(12)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.line_3 = QtGui.QFrame(self.tab_3)
+        self.line_3.setGeometry(QtCore.QRect(0, 40, 721, 16))
+        self.line_3.setFrameShape(QtGui.QFrame.HLine)
+        self.line_3.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line_3.setObjectName(_fromUtf8("line_3"))
+        self.tabWidget.addTab(self.tab_3, _fromUtf8(""))
+        self.tab_4 = QtGui.QWidget()
+        self.tab_4.setObjectName(_fromUtf8("tab_4"))
+        self.label_5 = QtGui.QLabel(self.tab_4)
+        self.label_5.setGeometry(QtCore.QRect(10, 10, 191, 20))
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Ubuntu"))
+        font.setPointSize(12)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName(_fromUtf8("label_5"))
+        self.line_4 = QtGui.QFrame(self.tab_4)
+        self.line_4.setGeometry(QtCore.QRect(0, 40, 721, 16))
+        self.line_4.setFrameShape(QtGui.QFrame.HLine)
+        self.line_4.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line_4.setObjectName(_fromUtf8("line_4"))
+        self.tabWidget.addTab(self.tab_4, _fromUtf8(""))
+        self.tab_5 = QtGui.QWidget()
+        self.tab_5.setObjectName(_fromUtf8("tab_5"))
+        self.label_6 = QtGui.QLabel(self.tab_5)
+        self.label_6.setGeometry(QtCore.QRect(10, 10, 261, 20))
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Ubuntu"))
+        font.setPointSize(12)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName(_fromUtf8("label_6"))
+        self.line_5 = QtGui.QFrame(self.tab_5)
+        self.line_5.setGeometry(QtCore.QRect(0, 40, 721, 16))
+        self.line_5.setFrameShape(QtGui.QFrame.HLine)
+        self.line_5.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line_5.setObjectName(_fromUtf8("line_5"))
+        self.tabWidget.addTab(self.tab_5, _fromUtf8(""))
+        self.gridLayout.addWidget(self.tabWidget, 2, 0, 1, 2)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.gridLayout.addLayout(self.horizontalLayout, 3, 1, 1, 1)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 3, 0, 1, 1)
+        self.comboBox = QtGui.QComboBox(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Ubuntu"))
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setWeight(50)
+        self.comboBox.setFont(font)
+        self.comboBox.setCursor(QtGui.QCursor(QtCore.Qt.UpArrowCursor))
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.gridLayout.addWidget(self.comboBox, 1, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
+
+        """Adding setStyleSheet to hide the tab"""
+        self.tabWidget.setStyleSheet("QTabWidget::tab-bar {width: 10; height: 0; margin: 0; padding: 0; border: none;} ")
+
+        """Adding function of the buttons and combo comboBox"""
+        #Cancel button to close the MainWindow
+        self.pushButton.clicked.connect(MainWindow.close)
+
+
+        """Adding the comboBox and tabWidget connection"""
+        QtCore.QObject.connect(self.comboBox, QtCore.SIGNAL(_fromUtf8("currentIndexChanged(int)")), self.tabWidget.setCurrentPage)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.label.setText(_translate("MainWindow", "CONNECTIONS", None))
+        self.label_2.setText(_translate("MainWindow", "Manage Citrix Connections", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Tab 1", None))
+        self.label_3.setText(_translate("MainWindow", "Manage VMware Connections", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Tab 2", None))
+        self.label_4.setText(_translate("MainWindow", "Manage RDP Connections", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "Page", None))
+        self.label_5.setText(_translate("MainWindow", "Manage SSH Connections", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("MainWindow", "Page", None))
+        self.label_6.setText(_translate("MainWindow", "Manage VNC Viewer Connections", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_5), _translate("MainWindow", "Page", None))
+        self.pushButton.setText(_translate("MainWindow", "CANCEL", None))
+        self.pushButton_2.setText(_translate("MainWindow", "OK", None))
+        self.comboBox.setItemText(0, _translate("MainWindow", "Citrix", None))
+        self.comboBox.setItemText(1, _translate("MainWindow", "VMware", None))
+        self.comboBox.setItemText(2, _translate("MainWindow", "RDP", None))
+        self.comboBox.setItemText(3, _translate("MainWindow", "SSH", None))
+        self.comboBox.setItemText(4, _translate("MainWindow", "VNC Viewer", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
